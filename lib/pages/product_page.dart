81a52e2450a4bf0f2d89ef3fe471673441bffc9d
Nam Tinnakorn RMU <tinnakorn.pei@rmu.ac.th>
2021-10-12 11:06:29 +0700

    Get Product API

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:my01_app/models/product.dart';
 import 'package:my01_app/widgets/menu.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class ProductPage extends StatefulWidget {
   const ProductPage({Key? key}) : super(key: key);
@@ -11,22 +14,60 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
+  List<Datum> course = [];
+
+  _getProduct() async {
+    var url = Uri.https('api.codingthailand.com', '/api/course');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      //print(response.body);
+      final Product product =
+          Product.fromJson(convert.jsonDecode(response.body));
+
+      setState(() {
+        course = product.data;
+      });
+    } else {
+      print("error api ${response.statusCode}");
+    }
+  }
+
   // ignore: prefer_typing_uninitialized_variables
-  var formAboutPage;
+  @override
+  void initState() {
+    // ignore: todo
+    // TODO: implement initState
+    super.initState();
+    _getProduct();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MenuItem(),
       appBar: AppBar(
-        title: const Text("สินค้า"),
+        title: const Text("Course"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[const Text("สินค้า")],
-        ),
-      ),
+      body: ListView.separated(
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                      image: NetworkImage(course[index].picture)),
+                ),
+              ),
+              title: Text(course[index].title),
+              subtitle: Text(course[index].detail),
+              trailing: const Icon(Icons.arrow_right_sharp),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+          itemCount: course.length),
     );
   }
 }
