866aa454c17f5fd4b97522cb8d0d679a13de2a2e
Nam Tinnakorn RMU <tinnakorn.pei@rmu.ac.th>
2021-10-14 14:18:08 +0700

    get api by FutureBuilder

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class AboutPage extends StatefulWidget {
   AboutPage({Key? key}) : super(key: key);
@@ -8,6 +10,25 @@ class AboutPage extends StatefulWidget {
 }
 
 class _AboutPageState extends State<AboutPage> {
+  Future<Map<String, dynamic>> _getData() async {
+    var url = Uri.https('api.codingthailand.com', '/api/version');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      //print(response.body);
+      final Map<String, dynamic> version = convert.jsonDecode(response.body);
+      return version;
+    } else {
+      throw Exception("Failed to load version ${response.statusCode}");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     final arguments = ModalRoute.of(context)!.settings.arguments as Map;
@@ -20,6 +41,19 @@ class _AboutPageState extends State<AboutPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            FutureBuilder<Map<String, dynamic>>(
+              future: _getData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data!['data']['version']);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),
             const Text("About Page"),
             const SizedBox(height: 30),
             Text(arguments['email']),
